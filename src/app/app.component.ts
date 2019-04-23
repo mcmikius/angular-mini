e2fc398d749d730b9712ac441f726f66d9bba948
Michail Bondarenko <mcmikius15@gmail.com>
2019-04-23 12:07:55 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+<<<<<<< HEAD
   styles: []
+=======
+  styleUrls: ['./app.component.css']
+>>>>>>> initial commit
 })
 export class AppComponent {
   title = 'angular-mini';
