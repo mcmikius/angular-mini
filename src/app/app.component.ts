74b8e8b5d93cde4b2ca2cb3f74233853fbdcd070
Michail Bondarenko <mcmikius15@gmail.com>
2019-05-03 12:42:13 +0300

    change merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-<<<<<<< HEAD
-  styles: []
-=======
   styleUrls: ['./app.component.css']
->>>>>>> initial commit
 })
 export class AppComponent {
   users = [
